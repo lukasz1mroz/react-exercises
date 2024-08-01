180df1f4e2d221733d7b8634a07efdd37f80e2ba
Lukasz Mroz <lukasz1mroz@gmail.com>
2024-08-01 07:55:37 +0200

    Use itemsReducer

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useEffect, useMemo, useReducer } from "react";
 import {useAuth} from "../hooks/useAuth";
+import {itemReducer} from "../reducers/itemsReducer";
+
+const initialTasks = {
+    items: [],
+    selectedItem: '',
+};
 
 const Items = () => {
     const {logout} = useAuth();
-    const [items, setItems] = useState([]);
-    const [error, setError] = useState(null);
+    const [state, dispatch] = useReducer(itemReducer, initialTasks);
+
 
     const fetchItems = () => {
         return new Promise((resolve, reject) => {
@@ -26,23 +32,52 @@ const Items = () => {
 
     useEffect(() => {
         fetchItems()
-            .then((data) => setItems(data))
+            .then((data) => dispatch({ type: 'set', items: data }))
             .catch((error) => {
                 console.error(error); 
-                setError(error)
             });
-    });
+    }, []);
 
-    // TODO: Implement useMemo to memoize filteredItems
-    // TODO: Implement reducer to manage items state
+    const handleChangeItem = (item) => {
+        dispatch({
+            type: 'selected',
+            item: item,
+        })
+    }
+
+    const handleDeleteItem = () => {
+        dispatch({
+            type: 'deleted',
+            item: state.selectedItem,
+        })
+    }
 
     const handleLogout = () => {
         logout();
     }
 
+    const filteredItems = useMemo(() => {
+        return state.items.filter(item => item.id === Number(state.selectedItem));
+    }, [state.items, state.selectedItem]);
+
     return (
         <>
             <h1>Items</h1>
+            <select value={state.selectedItem} onChange={(e) => handleChangeItem(e.target.value)}>
+                <option value="" disabled>Select an item</option>
+                {state.items.map((item) =>(
+                    <option key={item.id} value={item.id}>
+                        {item.name}
+                    </option>
+                ))}
+            </select>
+            {state.selectedItem && (
+                <div>
+                    <h2>Selected item: {filteredItems[0]?.name}</h2>
+                    <button onClick={handleDeleteItem}>Delete</button>
+                </div>
+            )}
+            <br/>
             <button onClick={handleLogout}>Logout</button>
         </>
     )
